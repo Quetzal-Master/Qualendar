06b46396485adb35df9bba988071ec89b71efa37
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-09-06 18:51:10 +0200

    Add Translation files

--- src/components/interfaces/PaperCalendar.jsx
+++ src/components/interfaces/PaperCalendar.jsx
@@ -1,35 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { RiHomeGearFill, RiLoginBoxLine } from "react-icons/ri";
-import { v4 as uuidv4 } from "uuid";
-import ApiCalendar from "react-google-calendar-api";
 import Modal from "../Modal";
 
-const calendarID = process.env.REACT_APP_CALENDAR_ID;
-const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
-
-const config = {
-	clientId: calendarID,
-	apiKey: apiKey,
-	scope: "https://www.googleapis.com/auth/calendar",
-	discoveryDocs: [
-		"https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-	],
-};
-
-const apiCalendar = new ApiCalendar(config);
-function PaperCalendar({
-	modalOpen,
-	handleLoginLogout,
-	close,
-	modalType,
-	tmpLog,
-	unsplashimg,
-	monthNames,
-	month,
-	year,
-	divJours,
-}) {
+function PaperCalendar({ modalOpen, handleLoginLogout, close, modalType, tmpLog, unsplashimg, monthNames, month, year, divJours }) {
 	const dayEntranceAnimation = {
 		hidden: { opacity: 1, scale: 0 },
 		visible: {
@@ -42,8 +16,8 @@ function PaperCalendar({
 		},
 	};
 
-	const [touchStart, setTouchStart] = React.useState(0);
-	const [touchEnd, setTouchEnd] = React.useState(0);
+	const [touchStart, setTouchStart] = useState(0);
+	const [touchEnd, setTouchEnd] = useState(0);
 
 	function handleTouchStart(e) {
 		setTouchStart(e.targetTouches[0].clientX);
@@ -79,13 +53,7 @@ function PaperCalendar({
 	return (
 		<div className="calendrier">
 			<AnimatePresence initial={false} exitBeforeEnter={true}>
-				{modalOpen && (
-					<Modal
-						modalOpen={modalOpen}
-						handleClose={close}
-						type={modalType}
-					/>
-				)}
+				{modalOpen && <Modal modalOpen={modalOpen} handleClose={close} type={modalType} />}
 			</AnimatePresence>
 			<motion.div
 				className="params"
@@ -97,22 +65,12 @@ function PaperCalendar({
 			>
 				{tmpLog === false ? <RiLoginBoxLine /> : <RiHomeGearFill />}
 			</motion.div>
-			<div
-				className="calendrierHeader"
-				onTouchStart={(e) => handleTouchStart(e)}
-				onTouchMove={(e) => handleTouchMove(e)}
-				onTouchEnd={(e) => handleTouchEnd(e)}
-			>
+			<div className="calendrierHeader" onTouchStart={(e) => handleTouchStart(e)} onTouchMove={(e) => handleTouchMove(e)} onTouchEnd={(e) => handleTouchEnd(e)}>
 				<img src={unsplashimg.src} alt={unsplashimg.alt} />
 				<h1>{monthNames[month]}</h1>
 				<h3>{year}</h3>
 			</div>
-			<motion.div
-				variants={dayEntranceAnimation}
-				initial="hidden"
-				animate="visible"
-				className="calendrierBody"
-			>
+			<motion.div variants={dayEntranceAnimation} initial="hidden" animate="visible" className="calendrierBody">
 				{divJours}
 			</motion.div>
 		</div>
