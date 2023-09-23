867bac5ff0422230051f2ee10ba2d3838aaaee74
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-09-23 16:24:40 +0200

    Fix building error

--- src/components/interfaces/PaperCalendar.jsx
+++ src/components/interfaces/PaperCalendar.jsx
@@ -4,7 +4,7 @@ import { RiHomeGearFill, RiLoginBoxLine } from "react-icons/ri";
 import { monthsList } from "@/constants/dayMonth";
 import getUnsplashImage from "@/hooks/ImageUnsplash";
 import { useDispatch, useSelector } from "react-redux";
-import { setCurrentMonth } from "@/stores/calendar/calendarSlice";
+import { setCurrentMonth } from "@/stores/calendar/CalendarSlice";
 import { toggleIsLogged } from "../../stores/user/UserSlice";
 
 const dayEntranceAnimation = {
@@ -21,9 +21,7 @@ const dayEntranceAnimation = {
 
 function PaperCalendar({ apiCalendar, divJours }) {
 	const dispatch = useDispatch();
-	const { currentMonth, currentYear } = useSelector(
-		(state) => state.calendar
-	);
+	const { currentMonth, currentYear } = useSelector((state) => state.calendar);
 	const { isLogged } = useSelector((state) => state.user);
 
 	const [touchStart, setTouchStart] = useState(0);
@@ -59,21 +57,12 @@ function PaperCalendar({ apiCalendar, divJours }) {
 			>
 				{isLogged === false ? <RiLoginBoxLine /> : <RiHomeGearFill />}
 			</motion.div>
-			<div
-				className="calendrierHeader"
-				onTouchStart={(e) => setTouchStart(e.targetTouches[0].screenX)}
-				onTouchEnd={(e) => handleTouchEnd(e.changedTouches[0].screenX)}
-			>
+			<div className="calendrierHeader" onTouchStart={(e) => setTouchStart(e.targetTouches[0].screenX)} onTouchEnd={(e) => handleTouchEnd(e.changedTouches[0].screenX)}>
 				<img src={unsplashimg.src} alt={unsplashimg.alt} />
 				<h1>{monthsList[currentMonth]}</h1>
 				<h3>{currentYear}</h3>
 			</div>
-			<motion.div
-				variants={dayEntranceAnimation}
-				initial="hidden"
-				animate="visible"
-				className="calendrierBody"
-			>
+			<motion.div variants={dayEntranceAnimation} initial="hidden" animate="visible" className="calendrierBody">
 				{divJours}
 			</motion.div>
 		</div>
